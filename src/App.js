f05cc59471b7f5c886c9fadfc6cd5e9aeb6f4040
mekams <kamraneg@gmail.com>
2024-07-06 02:06:45 +0100

    footer-bg color fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import Layout from './components/Layout.jsx';
+import React, { useState, useEffect } from 'react';
+import Loader from './components/Loader.jsx';
+
+
 function App() {
-  return <Layout />;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a network request or any async operation
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);  // Adjust the timeout as needed
+  }, []);
+  return (
+    <>
+    <div className="App">
+      {isLoading ? <Loader/> :<Layout/>}
+    </div> 
+    </>
+  
+  )
+  ;
 }
 
 export default App;
---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/loader.css';  // Import your CSS file
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader">
+        {/* Loader styling and animation */}
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
