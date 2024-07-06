7117f5232078151f52eb921a7f85a2db7291b084
mekams <kamraneg@gmail.com>
2024-07-06 15:48:49 +0100

    layout adjusted

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Routes from '../routes/Router.jsx';
-import Carts from '../components/UI/ShopCart.jsx';
+import ShopCart from '../components/UI/ShopCart.jsx';
 import { useSelector } from 'react-redux';
 const Layout = () => {
   const showCart = useSelector((state) => state.cartUI.cartIsVisable);
@@ -10,7 +10,7 @@ const Layout = () => {
     <div>
       <Header />
 
-      {showCart && <Carts />}
+      {showCart && <ShopCart />}
       <div>
         <Routes />
       </div>
