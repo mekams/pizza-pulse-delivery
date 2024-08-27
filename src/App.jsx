2a23e5c5b6628b32b8c111aa08a5c06dbd300963
mekams <kamraneg@gmail.com>
2024-08-27 15:38:46 +0100

    lady loading enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
-import Layout from './components/Layout.jsx';
-import React, { useState, useEffect } from 'react';
+// import Layout from './components/Layout.jsx';
+// import React, { useState, useEffect } from 'react';
+// import Loader from './components/Loader.jsx';
+
+
+
+// function App() {
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     // Simulate a network request or any async operation
+//     setTimeout(() => {
+//       setIsLoading(false);
+//     }, 4000);  // Adjust the timeout as needed
+//   }, []);
+//   return (
+//     <>
+//     <div className="App">
+//       {isLoading ? <Loader/> :<Layout/>}
+//     </div> 
+//     </>
+  
+//   )
+//   ;
+// }
+
+// export default App;
+
+
+
+import React, { useState, useEffect, lazy, Suspense } from 'react';
 import Loader from './components/Loader.jsx';
 
-
+// Lazy load the Layout component
+const Layout = lazy(() => import('./components/Layout.jsx'));
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -13,15 +43,18 @@ function App() {
       setIsLoading(false);
     }, 4000);  // Adjust the timeout as needed
   }, []);
+
   return (
-    <>
     <div className="App">
-      {isLoading ? <Loader/> :<Layout/>}
-    </div> 
-    </>
-  
-  )
-  ;
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Suspense fallback={<Loader />}>
+          <Layout />
+        </Suspense>
+      )}
+    </div>
+  );
 }
 
 export default App;
