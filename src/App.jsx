11dcf4b29b58b941ed28fce716278c91eed34140
mekams <kamraneg@gmail.com>
2024-08-27 15:40:11 +0100

    lady loading enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,3 @@
-// import Layout from './components/Layout.jsx';
-// import React, { useState, useEffect } from 'react';
-// import Loader from './components/Loader.jsx';
-
-
-
-// function App() {
-//   const [isLoading, setIsLoading] = useState(true);
-
-//   useEffect(() => {
-//     // Simulate a network request or any async operation
-//     setTimeout(() => {
-//       setIsLoading(false);
-//     }, 4000);  // Adjust the timeout as needed
-//   }, []);
-//   return (
-//     <>
-//     <div className="App">
-//       {isLoading ? <Loader/> :<Layout/>}
-//     </div> 
-//     </>
-  
-//   )
-//   ;
-// }
-
-// export default App;
-
-
-
 import React, { useState, useEffect, lazy, Suspense } from 'react';
 import Loader from './components/Loader.jsx';
 
